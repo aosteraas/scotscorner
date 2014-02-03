8a10db1d603c4897ef9b0d0b86bf708d0997c93f
Aaron Osteraas <aaron.osteraas@gmail.com>
2014-02-03 12:49:55 +1100

    First commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php require_once('header.php'); ?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to Scots Corner</h1>
+        <p>Scots Corner is a radio show featuring Traditional Scottish music, Folk and Gaelic music, as well as Scottish News, Views, and Blethers.</p>
+        <p>Scots Corner is hosted by David Keith, broadcast 2:30pm to 4:30pm, every Monday on Pheonix FM</p>
+        <!-- <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p> -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>About David</h2>
+          <p>David Keith is a Scotsman by day, and doubly Scotsman by night.</p>
+          <p>He's been on the air with Scots Corner since 11th October, 1982, soothing adoring fans with his voice ever since.</p>
+          <p><a class="btn btn-default" href="/about.php" role="button">Learn More &raquo;</a></p>
+          <p><img src="img/sf.gif" class="img-responsive"></p>
+        </div>
+        <div class="col-md-4">
+          <h2>&nbsp;</h2>
+          <p><img src="img/dkeith.png" class="img-responsive"></p>
+          <!-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> -->
+       </div>
+        <div class="col-md-4">
+          <h2>More Information</h2>
+          <p><a href="/about.php">About David</a></p>
+          <p><a href="http://www.phoenixfm.org.au/livePlayer.html" target="_blank">Listen Live</a></p>
+          <p><a href="http://www.phoenixfm.org.au/">Pheonix FM</a></p>
+          <p><img src="img/lr.jpg" class="img-responsive"></p>
+          <!-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> -->
+        </div>
+
+      </div>
+
+      <hr>
+
+<?php require_once('footer.php'); ?>
