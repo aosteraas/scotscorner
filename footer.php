0fc1762ece236291a3eeadea3f6f088eb23f088c
Aaron Osteraas <aaron.osteraas@gmail.com>
2017-03-08 11:01:45 +1100

    updated footer to include curent year

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
       <footer>
-        <p>&copy; Scots Corner 2013</p>
+        <p>&copy; Scots Corner 2013 - <?php echo date("Y"); ?></p>
       </footer>
     </div> <!-- /container -->
   </body>
