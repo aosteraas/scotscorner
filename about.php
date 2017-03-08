890ee3f2bc80df541067683e251da36bbd5b7385
Aaron Osteraas <aaron.osteraas@gmail.com>
2017-03-08 11:02:41 +1100

    updated about page

--- about.php
+++ about.php
@@ -1,7 +1,11 @@
 <?php require_once('header.php'); ?>
     <div class="jumbotron">
       <div class="container">
-        <h1>About David Keith</h1>
+        <div class="row">
+          <div class="col-md-12">
+            <h1>About David Keith</h1>
+          </div>
+        </div>
       </div>
     </div>
     <div class="container">
@@ -9,15 +13,14 @@
         <div class="col-md-12">
           <h2>About David</h2>
           <p>I joined 3CCC shortly after it was formed, my first programme of “Scots Corner” was on the 11th.of October or November, 1982.</p>
-            <p>My introduction was a ten minute “ familiarization” of the Panel. The Panel consisted of two turntables and a tape deck, and of course the Cart machine. “Earphones on switch to cue, now wind back the record on the track that you want to play, and when you can’t hear anything you are cued up?”</p>
-            <p>Surprisingly enough, it went well, the programme started at 4:30 p.m., and the first one went for 30 minutes, this later changed to an hour and was always Monday afternoon.There came a time when the committee decided the programme would be better on a Sunday, however the calls from listeners suggested otherwise, so “Scots Corner” went back to Monday. The timeslot stayed for many years, and was increased to 90 minutes some time later. The programme is a real mix, of Scottish Folk, Traditional, a bit of Gaelic, and also featuring others like, K.T.Tunstall, Gerry Rafferty, Donovan, etc. There is always room for Scottish news, and events in Scotland.</p>
-            <p>The last two years has seen the development of "Mish­Mash" a progamme where anything goes, from Pavarotti, to the Beatles. This show is broadcast every Monday from 9:00am to 11:ooam. i would be pleased to welcome you to either of my shows. hear from you soon.</p>
-            <p>This was a magic time at 3CCC, there was a constant air of expectation, remember the hard working committee was breaking new ground,and kept busy ensuring that 3CCC was here to stay.</p>
-            <p>Jeff Langdon, the inaugural manager, left to take up a position in South Australia, and Gerry Pine took over, overseeing our move to Bendigo, where it was thought that we would have a larger profile, and be more accessible to a greater cross­ section of the public of central Victoria .Gerry managed the Station for over ten years, until he moved to other areas of technology. I am glad to say that both Jeff and Gerry are strong supporters of Phoenix fm.</p>
-            <p>An old Scottish song tells us that “ when the battle is o’er the war has just begun”</p>
-            <p>So it is then with Phoenix, we have come through the “ war”, and who would have thought, that after about two years, we would have such a vibrant radio station, with so many supporters, and a “Bevy” of great, committed presenters.</p>
-            <p>I feel blessed that I am still on Community Radio [Still on Monday] and part of a great group, many of whom put in many hours to ensure the longevity of Phoenix fm.</p>
-          
+          <p>My introduction was a ten minute “ familiarization” of the Panel. The Panel consisted of two turntables and a tape deck, and of course the Cart machine. “Earphones on switch to cue, now wind back the record on the track that you want to play, and when you can’t hear anything you are cued up?”</p>
+          <p>Surprisingly enough, it went well, the programme started at 4:30 p.m., and the first one went for 30 minutes, this later changed to an hour and was always Monday afternoon.There came a time when the committee decided the programme would be better on a Sunday, however the calls from listeners suggested otherwise, so “Scots Corner” went back to Monday. The timeslot stayed for many years, and was increased to 90 minutes some time later. The programme is a real mix, of Scottish Folk, Traditional, a bit of Gaelic, and also featuring others like, K.T.Tunstall, Gerry Rafferty, Donovan, etc. There is always room for Scottish news, and events in Scotland.</p>
+          <p>The last two years has seen the development of "Mish­Mash" a progamme where anything goes, from Pavarotti, to the Beatles. This show is broadcast every Monday from 9:00am to 11:ooam. i would be pleased to welcome you to either of my shows. hear from you soon.</p>
+          <p>This was a magic time at 3CCC, there was a constant air of expectation, remember the hard working committee was breaking new ground,and kept busy ensuring that 3CCC was here to stay.</p>
+          <p>Jeff Langdon, the inaugural manager, left to take up a position in South Australia, and Gerry Pine took over, overseeing our move to Bendigo, where it was thought that we would have a larger profile, and be more accessible to a greater cross­ section of the public of central Victoria .Gerry managed the Station for over ten years, until he moved to other areas of technology. I am glad to say that both Jeff and Gerry are strong supporters of Phoenix fm.</p>
+          <p>An old Scottish song tells us that “ when the battle is o’er the war has just begun”</p>
+          <p>So it is then with Phoenix, we have come through the “ war”, and who would have thought, that after about two years, we would have such a vibrant radio station, with so many supporters, and a “Bevy” of great, committed presenters.</p>
+          <p>I feel blessed that I am still on Community Radio [Still on Monday] and part of a great group, many of whom put in many hours to ensure the longevity of Phoenix fm.</p>
         </div>
       </div>
       <hr>
